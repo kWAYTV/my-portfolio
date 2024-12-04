8e3a9bd007f38cd3fd9371bfa350f3a4ee792fff
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-12-04 18:17:15 +0100

    refactor: Simplify Footer component and update year display
    - Removed unnecessary state management and layout effect for the year display, directly using the current year.
    - Cleaned up class names by removing redundant properties for better readability.
    - Enhanced the structure of the footer links while maintaining accessibility features.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,45 +1,32 @@
-'use client';
-
 import { ArrowUpRight } from 'lucide-react';
-import { useLayoutEffect, useState } from 'react';
 
 import { githubRepoUrl } from '@/lib/metadata';
 
 export default function Footer() {
-  const [year, setYear] = useState<number | null>(null);
-
-  useLayoutEffect(() => {
-    setYear(new Date().getFullYear());
-  }, []);
-
   return (
     <footer className='mb-16'>
-      <ul className='font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 dark:text-neutral-300 md:flex-row md:space-x-4 md:space-y-0'>
+      <ul className='font-sm mt-8 flex flex-col space-y-2 text-neutral-600 dark:text-neutral-300 md:flex-row md:space-x-4 md:space-y-0'>
         <li>
           <a
-            className='flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100'
-            rel='noopener noreferrer'
-            target='_blank'
             href='/rss'
+            className='flex items-center hover:text-neutral-800 dark:hover:text-neutral-100'
           >
             <ArrowUpRight />
-            <p className='ml-2 h-7'>rss</p>
+            <p className='ml-2'>rss</p>
           </a>
         </li>
         <li>
           <a
-            className='flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100'
-            rel='noopener noreferrer'
-            target='_blank'
             href={githubRepoUrl}
+            className='flex items-center hover:text-neutral-800 dark:hover:text-neutral-100'
           >
             <ArrowUpRight />
-            <p className='ml-2 h-7'>repo</p>
+            <p className='ml-2'>repo</p>
           </a>
         </li>
       </ul>
       <p className='mt-8 text-neutral-600 dark:text-neutral-300'>
-        © {year} MIT Licensed
+        © {new Date().getFullYear()} MIT Licensed
       </p>
     </footer>
   );
