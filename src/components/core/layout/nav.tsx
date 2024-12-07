aca28c54ffa44fad5e226345c1f6a60c59ad68ce
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-12-07 14:18:12 +0100

    fix(nav): remove negative margin causing inconsistent navbar positioning

--- src/components/core/layout/nav.tsx
+++ src/components/core/layout/nav.tsx
@@ -14,7 +14,7 @@ import type { NavPath } from '@/types/nav';
 
 export function Navbar() {
   return (
-    <aside className='-ml-[8px] mb-16 tracking-tight'>
+    <aside className='mb-16 tracking-tight'>
       <div className='lg:sticky lg:top-20'>
         <nav
           className='fade relative flex scroll-pr-6 flex-row items-start px-0 pb-0 md:relative md:overflow-auto'
